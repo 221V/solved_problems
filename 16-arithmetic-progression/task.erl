3e1af5880bc64de97fb5eb0afb71a4728fb979a4
221V <221v92@gmail.com>
2016-12-03 23:34:51 +0200

    no message

---
+++ 16-arithmetic-progression/task.erl
@@ -0,0 +1,23 @@
+-module(task).
+
+-export([ar_pr/2]).
+
+ar_pr(Count, Values) ->
+  ar_pr(Count, Values, []).
+
+ar_pr(_, [], Result) ->
+  lists:reverse(Result);
+ar_pr(Count, [ValueHead|ValuesTail], Result) ->
+  {V1, V2, V3} = ValueHead,
+  Res1 = sum_pr(V3, V1, V2, 0),
+  ar_pr(Count - 1, ValuesTail, [erlang:integer_to_binary(Res1)|Result]).
+
+sum_pr(0, _, _, Sum) ->
+  Sum;
+sum_pr(Count, Begin, Step, Sum) ->
+  if
+    Count > 1 ->
+      sum_pr(Count - 1, Begin, Step, Sum + (Begin + Step * (Count - 1)));
+    true ->
+      sum_pr(Count - 1, Begin, Step, Sum + Begin)
+  end.
---
+++ 16-arithmetic-progression/task.ex
@@ -0,0 +1,29 @@
+defmodule Task do
+
+  def ar_pr(count, values) do
+    ar_pr(count, values, [])
+  end
+
+  defp ar_pr(_, [], result) do
+    Enum.reverse(result)
+  end
+
+  defp ar_pr(count, [head_value|tail_values], result) do
+    {v1, v2, v3} = head_value
+    res1 = sum_pr(v3, v1, v2, 0)
+    ar_pr(count - 1, tail_values, [Integer.to_string(res1)|result])
+  end
+
+  defp sum_pr(0, _, _, sum) do
+    sum
+  end
+  
+  defp sum_pr(count, begin, step, sum) do
+    if count > 1 do
+      sum_pr(count - 1, begin, step, sum + (begin + step * (count - 1)))
+    else
+      sum_pr(count - 1, begin, step, sum + begin)
+    end
+  end
+
+end
